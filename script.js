92f9bdad34c0e6ac5b414f00aee03c50dceec143
Rohithmunna31 <rohihkasna31@gmail.com>
2023-11-19 12:26:33 +0530

    fifthcommit

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ item[2].style.backgroundColor = 'green';
 for(let i=0;i<item.length;i++){
     item[i].style.fontWeight = 'bold';
 }        
+
+var li = getElementByTagName('li');
+li[2].style.backgroundColor = 'black';
+
+li[4].style.fontcolor = 'red';
+// if we get it by tag name even if we dont have class name we can edit it 
+// using tag name 
