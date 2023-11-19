e0f332fb1024587927d2fb95e08af8b5f49b1d4e
Rohithmunna31 <rohihkasna31@gmail.com>
2023-11-19 12:55:52 +0530

    sixth commit

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ li[2].style.backgroundColor = 'black';
 li[4].style.fontcolor = 'red';
 // if we get it by tag name even if we dont have class name we can edit it 
 // using tag name 
+
+
+
+// now changing using query selector
+var quer = document.querySelector('.list-group-item');
+
+quer.style.backgroundColor = 'green';
+
+var secondchild = document.querySelector('.list-group-item:nth-child(2)');
+
+secondchild.style.backgroundColor = 'green';
+
+var thirdchild = document.querySelector('.list.group.item:nth-child(3)');
+
+thirdchild.style.display = 'none';
+
+var allitem = document.querySelectorAll('.list-group-item');
+
+allitem[1].style.color = 'green';
+
+var odditems = document.querySelectorAll('li:nth-child(odd)');
+
+for(let i=0;i<odditems.length;i++){
+    odditems[i].style.backgroundColor = 'green';
+}
